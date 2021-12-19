a8fade442581d1125ca6a72adec186b335c9221c
moaikang <moaikang.dev@gmail.com>
2021-12-20 00:17:57 +0900

    아이디어 구상, 프로젝트 실현 가능성 검토 중 (1)
    - 블로그를 만들어보자고 생각했음.
    - 막상 만들기엔 너무 게으른 내 자신을 발견.
    - 마크다운 파일을 추가하면 블로그 처럼 README.md를 적어주는 스크립트를 짜보는게 어떨까 하는 아이디어 생각해봄.
    - 이에 따라 프로젝트를 구상해 보는 중.

---
+++ README.md
@@ -0,0 +1,23 @@
+# DEV_MOAI 🧑‍💻<br>
+
+---
+
+<center>
+Category
+</center>
+
+### Web
+
+<a href="./posts/Web/오오.md">오오</a><br>
+
+### Retrospect
+
+###
+
+---
+
+<center>
+© Keunwoo Kang<br>
+  <a target="_blank" href="mailto:moaikang.dev@gmail.com?subject=Hello%20Ileri,%20From%20Github"><img src="https://img.shields.io/badge/gmail-%23D14836.svg?&style=flat-square&logo=gmail&logoColor=white" /></a>
+  <br>
+</center>
---
+++ scripts/constants.js
@@ -0,0 +1,20 @@
+const Header = `# DEV_MOAI 🧑‍💻<br>
+---
+`;
+
+const Footer = `
+---
+
+<center>
+© Keunwoo Kang<br>
+  <a target="_blank" href="mailto:moaikang.dev@gmail.com?subject=Hello%20Ileri,%20From%20Github"><img src="https://img.shields.io/badge/gmail-%23D14836.svg?&style=flat-square&logo=gmail&logoColor=white" /></a>
+  <br>
+</center>
+`;
+
+const constants = {
+  Header,
+  Footer,
+};
+
+export default constants;
