1118332332d5318e1e0c204eeb0b8894ba4e5ac1
moaikang <moaikang.dev@gmail.com>
2021-12-27 00:23:43 +0900

    푸터 얼라인 가운데로

--- scripts/constants.js
+++ scripts/constants.js
@@ -4,11 +4,11 @@ const Header = `# DEV_MOAI 🧑‍💻<br>
 const Footer = `
 ---
 
-<center>
+<div align="center">
 © Keunwoo Kang<br>
   <a target="_blank" href="mailto:moaikang.dev@gmail.com?subject=Hello%20Ileri,%20From%20Github"><img src="https://img.shields.io/badge/gmail-%23D14836.svg?&style=flat-square&logo=gmail&logoColor=white" /></a>
   <br>
-</center>
+</div>
 `;
 
 const CategoryHeader = `
@@ -17,22 +17,10 @@ Category
 </center>
 `.trim();
 
-const buildCategory = (categoryName) => {
-  return `
-  <details>
-    <summary>${categoryName}</summary>
-    <div markdown="1">😎숨겨진 내용😎</div>
-    <div markdown="2">ㅋㅋ</div>
-    <div markdown="2">ㅋㅋ</div>
-  </details>
-  `.trim();
-};
-
 const constants = {
   Header,
   Footer,
   CategoryHeader,
-  buildCategory,
 };
 
 module.exports = constants;
